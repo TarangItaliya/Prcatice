d9be07948172d694ad3eda4545434df142d826a1
TarangItaliya <132134114+TarangItaliya@users.noreply.github.com>
2023-06-28 14:49:20 +0530

    your commit msg

--- App.js
+++ App.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <div className='main_app'>App
+    </div>
+    <div>this is a app page in react js</div>
+    </>
   )
 }
 
